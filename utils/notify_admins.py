a62a9864453186508a789213e0386a89032fa3dc
Amirbek Ismatov <110034844+ismatovamirbek@users.noreply.github.com>
2025-03-19 16:55:41 +0500

    Update notify_admins.py

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,14 +1,15 @@
 import logging
-
+import asyncio
 from aiogram import Dispatcher
-
 from data.config import ADMINS
-
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Bot ishga tushdi")
+            message = await dp.bot.send_message(admin, "Bot ishga tushdi")  # Send a message
+            await asyncio.sleep(5)  # Wait for 5 seconds
+            await dp.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)  # Delete message
 
         except Exception as err:
-            logging.exception(err)
+            logging.exception(err)  # Log the error properly
+
